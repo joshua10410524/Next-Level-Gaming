e8afb49a67b8d80330b2b9505db0767813c90fe8
CarlonKettledas <ST10512491@myemeris.edu.za>
2026-09-25 16:46:52 +0200

    updated app.tsx

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Name = "Next Level Gaming";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <view style={styles.card}></view>
+      <Text>{Name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#101010',
+    paddingTop: 90,
+    paddingHorizontal: 20,
+  },
+  card:{
+    backgroundColor:"#f5f4ef",
+    padding:24,
+  },
+  name:{
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#f5f4ef",
   },
 });
